78753f0e333c565aa937959656a3bb77c7c26c4e
PenghuiJiao <penghui.jiao@intel.com>
2022-09-20 11:48:40 +0800

    [POAE7-2301]fix some suggestions

--- cider/util/Regexp.cpp
+++ cider/util/Regexp.cpp
@@ -44,7 +44,7 @@ extern "C" RUNTIME_EXPORT bool regexp_like(const char* str,
     boost::regex re(pattern, pat_len, boost::regex::extended);
     boost::cmatch what;
     result = boost::regex_match(str, str + str_len, what, re);
-  } catch (std::exception& error) {
+  } catch (std::runtime_error& error) {
     // LOG(ERROR) << "Regexp match error: " << error.what();
     result = false;
   }
