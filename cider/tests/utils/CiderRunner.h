bd8eb20e7d843f2b01272b52437da25b85cf35ef
Kunshang Ji <kunshang.ji@intel.com>
2022-09-20 13:29:23 +0800

    add ciderRunner and test

---
+++ cider/tests/utils/CiderRunner.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2022 Intel Corporation.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef MODULARSQL_CIDERRUNNER_H
+#define MODULARSQL_CIDERRUNNER_H
+
+#include "Utils.h"
+#include "cider/CiderBatch.h"
+#include "cider/CiderCompileModule.h"
+#include "cider/CiderRuntimeModule.h"
+
+#include <memory>
+
+class CiderCompiler {
+ public:
+  CiderCompiler();
+  ~CiderCompiler(){};
+
+  std::shared_ptr<CiderCompilationResult> compile(
+      const std::string& create_ddl,
+      const std::string& file_or_sql,
+      CiderCompilationOption cco = CiderCompilationOption::defaults(),
+      CiderExecutionOption ceo = CiderExecutionOption::defaults());
+
+ private:
+  std::shared_ptr<CiderCompileModule> cider_compile_module_;
+};
+
+class CiderRunner {
+ public:
+  CiderRunner(){};
+  virtual ~CiderRunner(){};
+  static std::shared_ptr<CiderRunner> createCiderRunner(
+      std::shared_ptr<CiderCompilationResult> res);
+  virtual std::shared_ptr<CiderBatch> processNextBatch(
+      const std::shared_ptr<CiderBatch>& input_batch) = 0;
+  virtual std::shared_ptr<CiderBatch> finish() = 0;
+  virtual bool isFinished() {return finished_;}
+
+ protected:
+  std::shared_ptr<CiderCompilationResult> com_res_;
+  std::shared_ptr<CiderRuntimeModule> cider_runtime_module_;
+  bool finished_ = true;
+
+ private:
+};
+
+class CiderStatefulRunner : public CiderRunner {
+ public:
+  ~CiderStatefulRunner(){};
+  CiderStatefulRunner(std::shared_ptr<CiderCompilationResult> res);
+  std::shared_ptr<CiderBatch> finish() override;  // do nothing, return a nullptr;
+  std::shared_ptr<CiderBatch> processNextBatch(
+      const std::shared_ptr<CiderBatch>& input_batch) override;
+};
+
+class CiderStatelessRunner : public CiderRunner {
+ public:
+  ~CiderStatelessRunner(){};
+  CiderStatelessRunner(std::shared_ptr<CiderCompilationResult> res);
+  std::shared_ptr<CiderBatch> finish() override;
+  std::shared_ptr<CiderBatch> processNextBatch(
+      const std::shared_ptr<CiderBatch>& input_batch) override;
+};
+
+#endif  // MODULARSQL_CIDERRUNNER_H
---
+++ cider/tests/utils/CiderRunnerTest.cpp
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2022 Intel Corporation.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <string>
+
+#include "CiderRunner.h"
+#include "QueryDataGenerator.h"
+
+std::string create_ddl() {
+  return R"(CREATE TABLE test(col_1 INTEGER, col_2 BIGINT,
+  col_3 FLOAT, col_4 DOUBLE);)";
+}
+
+std::shared_ptr<CiderBatch> create_default_batch() {
+  return std::make_shared<CiderBatch>(
+      QueryDataGenerator::generateBatchByTypes(1000,
+                                               {"col_1", "col_2", "col_3", "col_4"},
+                                               {CREATE_SUBSTRAIT_TYPE(I32),
+                                                CREATE_SUBSTRAIT_TYPE(I64),
+                                                CREATE_SUBSTRAIT_TYPE(Fp32),
+                                                CREATE_SUBSTRAIT_TYPE(Fp64)}));
+}
+
+TEST(CiderRunnerTest, statelessRunnerMultiBatchTest) {
+  CiderCompiler compiler;
+  std::string ddl = create_ddl();
+  std::string sql = "SELECT col_1 FROM test WHERE col_1 > 0 ";
+  auto com_res = compiler.compile(ddl, sql);
+
+  auto input_batch = create_default_batch();
+
+  auto runner = CiderRunner::createCiderRunner(com_res);
+
+  EXPECT_TRUE(std::dynamic_pointer_cast<CiderStatelessRunner>(runner));
+
+  for (int i = 0; i < 1000000; i++) {
+    auto res = runner->processNextBatch(input_batch);
+  }
+  auto res_f = runner->finish();
+}
+
+TEST(CiderRunnerTest, statefulRunnerMultiBatchTest) {
+  CiderCompiler compiler;
+  std::string ddl = create_ddl();
+  std::string sql = "SELECT sum(col_1) FROM test WHERE col_1 > 0 ";
+  auto com_res = compiler.compile(ddl, sql);
+
+  auto input_batch = create_default_batch();
+
+  auto runner = CiderRunner::createCiderRunner(com_res);
+  // Agg query -> CiderStatefulRunner
+  EXPECT_TRUE(std::dynamic_pointer_cast<CiderStatefulRunner>(runner));
+
+  for (int i = 0; i < 1000000; i++) {
+    auto res = runner->processNextBatch(input_batch);
+  }
+  while (!runner->isFinished()) {
+    auto res_f = runner->finish();
+    EXPECT_EQ(res_f->row_num(), 1);
+  }
+}
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+
+  int err{0};
+  try {
+    err = RUN_ALL_TESTS();
+  } catch (const std::exception& e) {
+  }
+  return err;
+}
