b548dd9b8e373f13d13cb4a1f36430e121de32f6
spevenhe <xinyi.he@intel.com>
2022-09-22 04:23:22 +0000

    add sum on conditional tests

--- cider/tests/functionality/CiderCaseWhenTest.cpp
+++ cider/tests/functionality/CiderCaseWhenTest.cpp
@@ -75,17 +75,15 @@ class CiderCaseWhenRandomWithNullTestBase : public CiderTestBase {
 };
 
 TEST_F(CiderCaseWhenRandomWithNullTestBase, coalesceFunctionTest) {
-  assertQueryRowEqual("select coalesce(COL_INT, COL_BIGINT, 7) from test",
-                      "functions/conditional/coalesce.json");
-  assertQueryRowEqual("select coalesce(COL_INT, COL_BIGINT, COL_DOUBLE) from test",
-                      "functions/conditional/coalesce_null.json");
-  assertQueryRowEqual(
-      "select sum(coalesce(COL_INT, COL_BIGINT, COL_DOUBLE, 7)) from test",
-      "functions/conditional/coalesce_sum.json");
+  assertQuery("select coalesce(COL_INT, COL_BIGINT, 7) from test",
+              "functions/conditional/coalesce.json");
+  assertQuery("select coalesce(COL_INT, COL_BIGINT, COL_DOUBLE) from test",
+              "functions/conditional/coalesce_null.json");
+  assertQuery("select sum(coalesce(COL_INT, COL_BIGINT, COL_DOUBLE, 7)) from test",
+              "functions/conditional/coalesce_sum.json");
 }
 
-// substrait does not supports COALESCE functions.
-// and the COALESCE expression is a syntactic shortcut for the CASE expression
+// COALESCE expression is a syntactic shortcut for the CASE expression
 // the code COALESCE(expression1,...n) is same as the following CASE expression:
 // CASE
 // WHEN (expression1 IS NOT NULL) THEN expression1
@@ -94,15 +92,13 @@ TEST_F(CiderCaseWhenRandomWithNullTestBase, coalesceFunctionTest) {
 // ELSE expressionN
 // END
 TEST_F(CiderCaseWhenRandomWithNullTestBase, coalesceTest) {
-  assertQueryRowEqual(
-      "SELECT COALESCE(col_int, col_bigint, 777) FROM test",
-      "SELECT CASE WHEN col_int is not null THEN col_int WHEN col_bigint is not "
-      "null THEN col_bigint ELSE 777 END from test");
-  assertQueryRowEqual(
-      "SELECT COALESCE(col_int, col_double, 777) FROM test",
-      "SELECT CASE WHEN col_int is not null THEN col_int WHEN col_double is not "
-      "null THEN col_double ELSE 777 END from test");
-  assertQueryRowEqual(
+  assertQuery("SELECT COALESCE(col_int, col_bigint, 777) FROM test",
+              "SELECT CASE WHEN col_int is not null THEN col_int WHEN col_bigint is not "
+              "null THEN col_bigint ELSE 777 END from test");
+  assertQuery("SELECT COALESCE(col_int, col_double, 777) FROM test",
+              "SELECT CASE WHEN col_int is not null THEN col_int WHEN col_double is not "
+              "null THEN col_double ELSE 777 END from test");
+  assertQuery(
       "SELECT COALESCE(col_int, col_bigint, col_double, 777) FROM test",
       "SELECT CASE WHEN col_int is not null THEN col_int WHEN col_bigint is not null "
       "THEN col_bigint "
@@ -110,18 +106,17 @@ TEST_F(CiderCaseWhenRandomWithNullTestBase, coalesceTest) {
 }
 
 TEST_F(CiderCaseWhenRandomWithNullTestBase, coalesceAllArgumentsAreNullTest) {
-  assertQueryRowEqual(
+  assertQuery(
       "SELECT COALESCE(col_float) FROM test",
       "SELECT CASE WHEN col_float is not null THEN col_float ELSE null END from test");
-  assertQueryRowEqual(
+  assertQuery(
       "SELECT COALESCE(col_double, col_int) FROM test",
       "SELECT CASE WHEN col_double is not null THEN col_double WHEN col_int is not "
       "null THEN col_int ELSE null END from test");
-  assertQueryRowEqual(
-      "SELECT COALESCE(col_int, col_double) FROM test",
-      "SELECT CASE WHEN col_int is not null THEN col_int WHEN col_double is not "
-      "null THEN col_double ELSE null END from test");
-  assertQueryRowEqual(
+  assertQuery("SELECT COALESCE(col_int, col_double) FROM test",
+              "SELECT CASE WHEN col_int is not null THEN col_int WHEN col_double is not "
+              "null THEN col_double ELSE null END from test");
+  assertQuery(
       "SELECT COALESCE(col_int, col_bigint, col_double, col_float) FROM test",
       "SELECT CASE WHEN col_int is not null THEN col_int WHEN col_bigint is not "
       "null THEN col_bigint WHEN col_double is not null THEN col_double WHEN col_float "
@@ -130,28 +125,48 @@ TEST_F(CiderCaseWhenRandomWithNullTestBase, coalesceAllArgumentsAreNullTest) {
 }
 
 TEST_F(CiderCaseWhenRandomWithNullTestBase, coalesceWithAggTest) {
-  assertQueryRowEqual(
+  assertQuery(
       "SELECT sum(COALESCE(col_int, 777)) FROM test",
       "SELECT sum(CASE WHEN col_int is not null THEN col_int ELSE 777 END) from test");
-  assertQueryRowEqual("SELECT sum(COALESCE(col_double)) FROM test",
-                      "SELECT sum(CASE WHEN col_double is not null THEN col_double ELSE "
-                      "null END) from test");
-  assertQueryRowEqual("SELECT count(COALESCE(col_double, 777)) FROM test",
-                      "SELECT count(CASE WHEN col_double is not null THEN col_double "
-                      "ELSE 777 END) from test");
-  assertQueryRowEqual("SELECT sum(COALESCE(col_double, 777)) FROM test",
-                      "SELECT sum(CASE WHEN col_double is not null THEN col_double ELSE "
-                      "777 END) from test");
-  assertQueryRowEqual(
+  assertQuery("SELECT sum(COALESCE(col_double)) FROM test",
+              "SELECT sum(CASE WHEN col_double is not null THEN col_double ELSE "
+              "null END) from test");
+  assertQuery("SELECT count(COALESCE(col_double, 777)) FROM test",
+              "SELECT count(CASE WHEN col_double is not null THEN col_double "
+              "ELSE 777 END) from test");
+  assertQuery("SELECT sum(COALESCE(col_double, 777)) FROM test",
+              "SELECT sum(CASE WHEN col_double is not null THEN col_double ELSE "
+              "777 END) from test");
+  assertQuery(
       "SELECT sum(COALESCE(col_int, col_bigint, col_double, 777)) FROM test",
       "SELECT sum(CASE WHEN col_int is not null THEN col_int WHEN col_bigint is not null "
       "THEN "
       "col_bigint WHEN col_double is not null THEN col_double ELSE 777 END) from test");
-  assertQueryRowEqual(
+  assertQuery(
       "SELECT sum(COALESCE(col_int, col_bigint, col_double)) FROM test",
       "SELECT sum(CASE WHEN col_int is not null THEN col_int WHEN col_bigint is not null "
       "THEN "
       "col_bigint WHEN col_double is not null THEN col_double ELSE null END) from test");
+}
+
+// The IF function is actually a language construct that is equivalent to the following
+// CASE expression: CASE
+//    WHEN condition THEN true_value
+//    [ ELSE false_value ]
+// END
+// The Scalar Function IF from presto will be transalted to if-then expression in
+// substrait plan to cider, so tests are between IF function and case when
+TEST_F(CiderCaseWhenRandomWithNullTestBase, ScalarFunctionIF) {
+  assertQuery("SELECT IF(col_int = 1 ,10, 1) FROM test",
+              "SELECT CASE WHEN col_int = 1 THEN 10 ELSE 1 END FROM test");
+  assertQuery(
+      "SELECT IF(col_int > 20, col_bigint, col_double) FROM test",
+      "SELECT CASE WHEN col_int > 20 THEN col_bigint ELSE col_double END FROM test");
+  assertQuery("SELECT SUM( IF(col_int > 20, col_double, 5)) FROM test",
+              "SELECT SUM(CASE WHEN col_int > 20 THEN col_double ELSE 5 END) FROM test");
+  assertQuery(
+      "SELECT SUM(IF(col_double > 20, 5, col_double)) FROM test",
+      "SELECT SUM(CASE WHEN col_double > 20 THEN 5 ELSE col_double END) FROM test");
 }
 
 TEST_F(CiderCaseWhenSequenceTestBase, simpleCaseWhenTest) {
@@ -280,6 +295,16 @@ TEST_F(CiderCaseWhenRandomWithNullTestBase, CaseWhenAggTest) {
   assertQuery(
       "SELECT SUM(CASE WHEN col_int > 5 THEN 4 ELSE 3 END), CASE WHEN col_int > "
       "5 THEN 4 ELSE 3 END FROM test GROUP BY CASE WHEN col_int > 5 THEN 4 ELSE 3 END");
+  assertQuery(
+      "SELECT count(1), SUM(CASE WHEN col_int = 7 THEN col_bigint END), SUM(CASE WHEN "
+      "col_int = "
+      "5 THEN col_bigint + 3 END), SUM(CASE WHEN col_int = 7 THEN col_double * 2 ELSE "
+      "col_double / 2 END) "
+      "FROM test");
+  assertQuery(
+      "SELECT col_int,  SUM(CASE WHEN col_int = 5 THEN col_bigint + 3 END), SUM(CASE "
+      "WHEN col_int = 7 THEN col_double * 2 ELSE col_double / 2 END) FROM test GROUP BY "
+      "col_int");
 }
 
 int main(int argc, char** argv) {
