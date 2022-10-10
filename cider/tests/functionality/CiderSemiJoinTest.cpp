80d1ba99c9d0f5a983a38980d884e17b4f7d6216
Kunshang Ji <kunshang.ji@intel.com>
2022-10-10 12:31:16 +0800

    rename col name

--- cider/tests/functionality/CiderSemiJoinTest.cpp
+++ cider/tests/functionality/CiderSemiJoinTest.cpp
@@ -27,34 +27,33 @@ class CiderSemiJoinTest : public CiderJoinTestBase {
   CiderSemiJoinTest() {
     table_name_ = "table_probe";
     create_ddl_ =
-        "CREATE TABLE table_probe(l_a BIGINT, l_b INTEGER, l_c DOUBLE, l_d FLOAT, l_e "
-        "BOOLEAN);";
-    input_ = {std::make_shared<CiderBatch>(
-        QueryDataGenerator::generateBatchByTypes(500,
-                                                 {"l_a", "l_b", "l_c", "l_d", "l_e"},
-                                                 {CREATE_SUBSTRAIT_TYPE(I64),
-                                                  CREATE_SUBSTRAIT_TYPE(I32),
-                                                  CREATE_SUBSTRAIT_TYPE(Fp64),
-                                                  CREATE_SUBSTRAIT_TYPE(Fp32),
-                                                  CREATE_SUBSTRAIT_TYPE(Bool)}))};
+        "CREATE TABLE table_probe(l_long BIGINT, l_int INTEGER, l_double DOUBLE, "
+        "l_float FLOAT, l_bool BOOLEAN);";
+    input_ = {std::make_shared<CiderBatch>(QueryDataGenerator::generateBatchByTypes(
+        500,
+        {"l_long", "l_int", "l_double", "l_float", "l_bool"},
+        {CREATE_SUBSTRAIT_TYPE(I64),
+         CREATE_SUBSTRAIT_TYPE(I32),
+         CREATE_SUBSTRAIT_TYPE(Fp64),
+         CREATE_SUBSTRAIT_TYPE(Fp32),
+         CREATE_SUBSTRAIT_TYPE(Bool)}))};
 
     build_table_name_ = "table_hash";
     build_table_ddl_ =
-        "CREATE TABLE table_hash(r_a BIGINT, r_b INTEGER, r_c DOUBLE, r_d FLOAT, r_e "
-        "BOOLEAN, r_f INTEGER);";
+        "CREATE TABLE table_hash(r_long BIGINT, r_int INTEGER, r_double DOUBLE, r_float "
+        "FLOAT, r_bool BOOLEAN);";
     resetHashTable();
   }
 
   void resetHashTable() override {
     build_table_.reset(new CiderBatch(QueryDataGenerator::generateBatchByTypes(
         100,
-        {"r_a", "r_b", "r_c", "r_d", "r_e", "r_f"},
+        {"r_long", "r_int", "r_double", "r_float", "r_bool"},
         {CREATE_SUBSTRAIT_TYPE(I64),
          CREATE_SUBSTRAIT_TYPE(I32),
          CREATE_SUBSTRAIT_TYPE(Fp64),
          CREATE_SUBSTRAIT_TYPE(Fp32),
-         CREATE_SUBSTRAIT_TYPE(Bool),
-         CREATE_SUBSTRAIT_TYPE(I32)})));
+         CREATE_SUBSTRAIT_TYPE(Bool)})));
     duckDbQueryRunner_.createTableAndInsertData(
         build_table_name_, build_table_ddl_, {build_table_});
   }
@@ -65,66 +64,71 @@ class CiderSemiJoinNullableTest : public CiderJoinTestBase {
   CiderSemiJoinNullableTest() {
     table_name_ = "table_probe";
     create_ddl_ =
-        "CREATE TABLE table_probe(l_a BIGINT, l_b INTEGER, l_c DOUBLE, l_d FLOAT, l_e "
-        "BOOLEAN);";
-    input_ = {std::make_shared<CiderBatch>(
-        QueryDataGenerator::generateBatchByTypes(10,
-                                                 {"l_a", "l_b", "l_c", "l_d", "l_e"},
-                                                 {CREATE_SUBSTRAIT_TYPE(I64),
-                                                  CREATE_SUBSTRAIT_TYPE(I32),
-                                                  CREATE_SUBSTRAIT_TYPE(Fp64),
-                                                  CREATE_SUBSTRAIT_TYPE(Fp32),
-                                                  CREATE_SUBSTRAIT_TYPE(Bool)}))};
+        "CREATE TABLE table_probe(l_long BIGINT, l_int INTEGER, l_double DOUBLE, "
+        "l_float FLOAT, l_bool BOOLEAN);";
+    input_ = {std::make_shared<CiderBatch>(QueryDataGenerator::generateBatchByTypes(
+        10,
+        {"l_long", "l_int", "l_double", "l_float", "l_bool"},
+        {CREATE_SUBSTRAIT_TYPE(I64),
+         CREATE_SUBSTRAIT_TYPE(I32),
+         CREATE_SUBSTRAIT_TYPE(Fp64),
+         CREATE_SUBSTRAIT_TYPE(Fp32),
+         CREATE_SUBSTRAIT_TYPE(Bool)}))};
 
     build_table_name_ = "table_hash";
     build_table_ddl_ =
-        "CREATE TABLE table_hash(r_a BIGINT, r_b INTEGER, r_c DOUBLE, r_d FLOAT, r_e "
-        "BOOLEAN, r_f INTEGER);";
+        "CREATE TABLE table_hash(r_long BIGINT, r_int INTEGER, r_double DOUBLE, r_float "
+        "FLOAT, r_bool BOOLEAN);";
     resetHashTable();
   }
 
   void resetHashTable() override {
     build_table_.reset(new CiderBatch(QueryDataGenerator::generateBatchByTypes(
         3,
-        {"r_a", "r_b", "r_c", "r_d", "r_e", "r_f"},
+        {"r_long", "r_int", "r_double", "r_float", "r_bool"},
         {CREATE_SUBSTRAIT_TYPE(I64),
          CREATE_SUBSTRAIT_TYPE(I32),
          CREATE_SUBSTRAIT_TYPE(Fp64),
          CREATE_SUBSTRAIT_TYPE(Fp32),
-         CREATE_SUBSTRAIT_TYPE(Bool),
-         CREATE_SUBSTRAIT_TYPE(I32)},
-        {2, 2, 2, 2, 2, 2})));
+         CREATE_SUBSTRAIT_TYPE(Bool)},
+        {2, 2, 2, 2, 2})));
     duckDbQueryRunner_.createTableAndInsertData(
         build_table_name_, build_table_ddl_, {build_table_});
   }
 };
 
 TEST_F(CiderSemiJoinTest, semi) {
-  std::string sql_semi = "SELECT l_a FROM table_probe SEMI JOIN table_hash ON l_a = r_a";
+  std::string sql_semi =
+      "SELECT l_long FROM table_probe SEMI JOIN table_hash ON l_long = r_long";
   std::string sql_semi_in =
-      "SELECT l_a FROM table_probe WHERE l_a IN (SELECT r_a FROM table_hash)";
+      "SELECT l_long FROM table_probe WHERE l_long IN (SELECT r_long FROM table_hash)";
   assertJoinQueryAndReset(sql_semi_in, "semi.json");
 }
 
 TEST_F(CiderSemiJoinTest, anti) {
-  std::string sql_anti = "SELECT l_a FROM table_probe ANTI JOIN table_hash ON l_a = r_a";
+  std::string sql_anti =
+      "SELECT l_long FROM table_probe ANTI JOIN table_hash ON l_long = r_long";
   std::string sql_anti_in =
-      "SELECT l_a FROM table_probe WHERE l_a NOT IN (SELECT r_a FROM table_hash)";
+      "SELECT l_long FROM table_probe WHERE l_long NOT IN (SELECT r_long FROM "
+      "table_hash)";
   assertJoinQueryAndReset(sql_anti_in, "anti.json");
 }
 
 TEST_F(CiderSemiJoinNullableTest, semi) {
-  std::string sql_semi = "SELECT l_a FROM table_probe SEMI JOIN table_hash ON l_a = r_a";
+  std::string sql_semi =
+      "SELECT l_long FROM table_probe SEMI JOIN table_hash ON l_long = r_long";
   std::string sql_semi_in =
-      "SELECT l_a FROM table_probe WHERE l_a IN (SELECT r_a FROM table_hash)";
+      "SELECT l_long FROM table_probe WHERE l_long IN (SELECT r_long FROM table_hash)";
   assertJoinQueryAndReset(sql_semi_in, "semi.json");
 }
 
 TEST_F(CiderSemiJoinNullableTest, anti) {
   GTEST_SKIP_("DuckDB not in NULL will filter all rows.");
-  std::string sql_anti = "SELECT l_a FROM table_probe ANTI JOIN table_hash ON l_a = r_a";
+  std::string sql_anti =
+      "SELECT l_long FROM table_probe ANTI JOIN table_hash ON l_long = r_long";
   std::string sql_anti_in =
-      "SELECT l_a FROM table_probe WHERE l_a NOT IN (SELECT r_a FROM table_hash)";
+      "SELECT l_long FROM table_probe WHERE l_long NOT IN (SELECT r_long FROM "
+      "table_hash)";
   assertJoinQueryAndReset(sql_anti_in, "anti.json");
 }
 
