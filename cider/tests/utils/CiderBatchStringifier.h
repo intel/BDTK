ca39ae59b29c589187d0a10b3405f58fe8a5324d
YBRua <borui.yang@intel.com>
2022-10-20 11:32:28 +0800

    fixed linting and formatting

--- cider/tests/utils/CiderBatchStringifier.h
+++ cider/tests/utils/CiderBatchStringifier.h
@@ -1,3 +1,24 @@
+/*
+ * Copyright (c) 2022 Intel Corporation.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 #ifndef CIDER_CIDERBATCHSTRINGIFIER_H
 #define CIDER_CIDERBATCHSTRINGIFIER_H
 
@@ -14,14 +35,14 @@ class StructBatchStringifier : public CiderBatchStringifier {
   std::vector<std::unique_ptr<CiderBatchStringifier>> child_stringifiers_;
 
  public:
-  StructBatchStringifier(CiderBatch* batch);
-  virtual std::string stringifyValueAt(CiderBatch* batch, int row_index) override;
+  explicit StructBatchStringifier(CiderBatch* batch);
+  std::string stringifyValueAt(CiderBatch* batch, int row_index) override;
 };
 
 template <typename T>
 class ScalarBatchStringifier : public CiderBatchStringifier {
  public:
-  virtual std::string stringifyValueAt(CiderBatch* batch, int row_index) override;
+  std::string stringifyValueAt(CiderBatch* batch, int row_index) override;
 };
 
 #endif
