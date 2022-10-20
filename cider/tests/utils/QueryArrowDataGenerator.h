43489c6219317dcea84bfc08dc56e8e5c03122a3
imajiayu <jiayu.ma@intel.com>
2022-10-20 11:22:09 +0000

    add QueryArrowDataGenerator

---
+++ cider/tests/utils/QueryArrowDataGenerator.h
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) 2022 Intel Corporation.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef CIDER_QUERYARROWDATAGENERATOR_H
+#define CIDER_QUERYARROWDATAGENERATOR_H
+
+#include <limits>
+#include <random>
+#include <string>
+#include "ArrowArrayBuilder.h"
+#include "Utils.h"
+#include "cider/CiderBatch.h"
+#include "cider/CiderTypes.h"
+#include "substrait/type.pb.h"
+
+enum GeneratePattern { Sequence, Random };
+
+#define GENERATE_AND_ADD_COLUMN(C_TYPE)                                       \
+  {                                                                           \
+    std::vector<C_TYPE> col_data;                                             \
+    std::vector<bool> null_data;                                              \
+    std::tie(col_data, null_data) =                                           \
+        value_min > value_max                                                 \
+            ? generateAndFillVector<C_TYPE>(row_num, pattern, null_chance[i]) \
+            : generateAndFillVector<C_TYPE>(                                  \
+                  row_num, pattern, null_chance[i], value_min, value_max);    \
+    builder = builder.addColumn<C_TYPE>(names[i], type, col_data, null_data); \
+    break;                                                                    \
+  }
+
+#define N_MAX std::numeric_limits<T>::max()
+
+#define N_MIN std::numeric_limits<T>::min()
+
+class QueryArrowDataGenerator {
+ public:
+  static void generateBatchByTypes(ArrowSchema*& schema,
+                                   ArrowArray*& array,
+                                   const size_t row_num,
+                                   const std::vector<std::string>& names,
+                                   const std::vector<::substrait::Type>& types,
+                                   std::vector<int32_t> null_chance = {},
+                                   GeneratePattern pattern = GeneratePattern::Sequence,
+                                   const int64_t value_min = 0,
+                                   const int64_t value_max = -1) {
+    if (null_chance.empty()) {
+      null_chance = std::vector<int32_t>(types.size(), 0);
+    }
+    ArrowArrayBuilder builder;
+    builder = builder.setRowNum(row_num);
+    for (auto i = 0; i < types.size(); ++i) {
+      ::substrait::Type type = types[i];
+      switch (type.kind_case()) {
+        case ::substrait::Type::KindCase::kI8:
+          GENERATE_AND_ADD_COLUMN(int8_t)
+        case ::substrait::Type::KindCase::kI16:
+          GENERATE_AND_ADD_COLUMN(int16_t)
+        case ::substrait::Type::KindCase::kI32:
+          GENERATE_AND_ADD_COLUMN(int32_t)
+        case ::substrait::Type::KindCase::kI64:
+          GENERATE_AND_ADD_COLUMN(int64_t)
+        case ::substrait::Type::KindCase::kFp32:
+          GENERATE_AND_ADD_COLUMN(float)
+        case ::substrait::Type::KindCase::kFp64:
+          GENERATE_AND_ADD_COLUMN(double)
+        default:
+          CIDER_THROW(CiderCompileException, "Type not supported.");
+      }
+    }
+
+    auto schema_array = builder.build();
+    schema = std::get<0>(schema_array);
+    array = std::get<1>(schema_array);
+  }
+
+ private:
+  template <typename T>
+  static std::tuple<std::vector<T>, std::vector<bool>> generateAndFillVector(
+      const size_t row_num,
+      const GeneratePattern pattern,
+      const int32_t
+          null_chance,  // Null chance for each column, -1 represents for unnullable
+                        // column, 0 represents for nullable column but all data is not
+                        // null, 1 represents for all rows are null, values >= 2 means
+                        // each row has 1/x chance to be null.
+      const T value_min = N_MIN,
+      const T value_max = N_MAX) {
+    std::vector<T> col_data(row_num);
+    std::vector<bool> null_data(row_num);
+    std::mt19937 rng(std::random_device{}());  // NOLINT
+    switch (pattern) {
+      case GeneratePattern::Sequence:
+        for (auto i = 0; i < row_num; ++i) {
+          null_data[i] = Random::oneIn(null_chance, rng) ? (col_data[i] = N_MIN, true)
+                                                         : (col_data[i] = i, false);
+        }
+        break;
+      case GeneratePattern::Random:
+        if (std::is_integral<T>::value) {
+          // default type is int32_t. should not replace with T due to cannot gen float
+          // type template. Same for below.
+          for (auto i = 0; i < col_data.size(); ++i) {
+            null_data[i] = Random::oneIn(null_chance, rng)
+                               ? (col_data[i] = N_MIN, true)
+                               : (col_data[i] = static_cast<T>(
+                                      Random::randInt64(value_min, value_max, rng)),
+                                  false);
+          }
+        } else if (std::is_floating_point<T>::value) {
+          for (auto i = 0; i < col_data.size(); ++i) {
+            null_data[i] = Random::oneIn(null_chance, rng)
+                               ? (col_data[i] = N_MIN, true)
+                               : (col_data[i] = static_cast<T>(
+                                      Random::randFloat(value_min, value_max, rng)),
+                                  false);
+          }
+        } else {
+          std::string str = "Unexpected type:";
+          str.append(typeid(T).name()).append(", could not generate data.");
+          CIDER_THROW(CiderCompileException, str);
+        }
+        break;
+    }
+    return std::make_tuple(col_data, null_data);
+  }
+};
+
+#endif  // CIDER_QUERYARROWDATAGENERATOR_H
---
+++ cider/tests/utils/QueryArrowDataGeneratorTest.cpp
@@ -0,0 +1,138 @@
+/*
+ * Copyright (c) 2022 Intel Corporation.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <gtest/gtest.h>
+#include "ArrowArrayBuilder.h"
+#include "CiderBatchChecker.h"
+#include "QueryArrowDataGenerator.h"
+#include "exec/plan/parser/TypeUtils.h"
+
+#define VEC \
+  { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+
+#define TEST_GEN_SINGLE_COLUMN(C_TYPE, S_TYPE)                                      \
+  {                                                                                 \
+    ArrowArray* actual_array = nullptr;                                             \
+    ArrowSchema* actual_schema = nullptr;                                           \
+    QueryArrowDataGenerator::generateBatchByTypes(                                  \
+        actual_schema, actual_array, 10, {"col"}, {CREATE_SUBSTRAIT_TYPE(S_TYPE)}); \
+    EXPECT_EQ(actual_array->length, 10);                                            \
+    EXPECT_EQ(actual_array->n_children, 1);                                         \
+    ArrowArray* expected_array = nullptr;                                           \
+    ArrowSchema* expected_schema = nullptr;                                         \
+    std::tie(expected_schema, expected_array) =                                     \
+        ArrowArrayBuilder()                                                         \
+            .addColumn<C_TYPE>("col_1", CREATE_SUBSTRAIT_TYPE(S_TYPE), VEC)         \
+            .build();                                                               \
+    EXPECT_EQ(expected_array->length, 10);                                          \
+    EXPECT_EQ(expected_array->n_children, 1);                                       \
+  }
+// EXPECT_TRUE(ArrowChecker::checkEq(expected_batch, actual_batch));
+#define TEST_GEN_SINGLE_COLUMN_RANDOM(S_TYPE)                                      \
+  {                                                                                \
+    ArrowArray* array = nullptr;                                                   \
+    ArrowSchema* schema = nullptr;                                                 \
+    QueryArrowDataGenerator::generateBatchByTypes(schema,                          \
+                                                  array,                           \
+                                                  10,                              \
+                                                  {"col"},                         \
+                                                  {CREATE_SUBSTRAIT_TYPE(S_TYPE)}, \
+                                                  {},                              \
+                                                  GeneratePattern::Random);        \
+    EXPECT_EQ(array->length, 10);                                                  \
+    EXPECT_EQ(array->n_children, 1);                                               \
+  }
+
+TEST(QueryDataGeneratorTest, genSingleColumn) {
+  TEST_GEN_SINGLE_COLUMN(int8_t, I8);
+  TEST_GEN_SINGLE_COLUMN(int16_t, I16);
+  TEST_GEN_SINGLE_COLUMN(int32_t, I32);
+  TEST_GEN_SINGLE_COLUMN(int64_t, I64);
+  TEST_GEN_SINGLE_COLUMN(float, Fp32);
+  TEST_GEN_SINGLE_COLUMN(double, Fp64);
+}
+
+TEST(QueryDataGeneratorTest, genSingleColumnRandom) {
+  ArrowArray* array = nullptr;
+  ArrowSchema* schema = nullptr;
+  QueryArrowDataGenerator::generateBatchByTypes(schema,
+                                                array,
+                                                10,
+                                                {"col"},
+                                                {CREATE_SUBSTRAIT_TYPE(I32)},
+                                                {},
+                                                GeneratePattern::Random);
+
+  EXPECT_EQ(array->length, 10);
+  EXPECT_EQ(array->n_children, 1);
+  EXPECT_NE(array->children[0], nullptr);
+}
+
+TEST(QueryDataGeneratorTest, randomData) {
+  TEST_GEN_SINGLE_COLUMN_RANDOM(I8);
+  TEST_GEN_SINGLE_COLUMN_RANDOM(I16);
+  TEST_GEN_SINGLE_COLUMN_RANDOM(I32);
+  TEST_GEN_SINGLE_COLUMN_RANDOM(I64);
+  TEST_GEN_SINGLE_COLUMN_RANDOM(Fp32);
+  TEST_GEN_SINGLE_COLUMN_RANDOM(Fp64);
+}
+
+TEST(QueryDataGeneratorTest, genMultiColumns) {
+  ArrowArray* actual_array = nullptr;
+  ArrowSchema* actual_schema = nullptr;
+  QueryArrowDataGenerator::generateBatchByTypes(
+      actual_schema,
+      actual_array,
+      10,
+      {"col_i8", "col_i16", "col_i32", "col_i64", "col_f32", "col_f64"},
+      {CREATE_SUBSTRAIT_TYPE(I8),
+       CREATE_SUBSTRAIT_TYPE(I16),
+       CREATE_SUBSTRAIT_TYPE(I32),
+       CREATE_SUBSTRAIT_TYPE(I64),
+       CREATE_SUBSTRAIT_TYPE(Fp32),
+       CREATE_SUBSTRAIT_TYPE(Fp64)});
+  EXPECT_EQ(actual_array->length, 10);
+  EXPECT_EQ(actual_array->n_children, 6);
+
+  ArrowArray* expected_array = nullptr;
+  ArrowSchema* expected_schema = nullptr;
+  std::tie(expected_schema, expected_array) =
+      ArrowArrayBuilder()
+          .addColumn<int8_t>("", CREATE_SUBSTRAIT_TYPE(I8), VEC)
+          .addColumn<int16_t>("", CREATE_SUBSTRAIT_TYPE(I16), VEC)
+          .addColumn<int32_t>("", CREATE_SUBSTRAIT_TYPE(I32), VEC)
+          .addColumn<int64_t>("", CREATE_SUBSTRAIT_TYPE(I64), VEC)
+          .addColumn<float>("", CREATE_SUBSTRAIT_TYPE(Fp32), VEC)
+          .addColumn<double>("", CREATE_SUBSTRAIT_TYPE(Fp64), VEC)
+          .build();
+  // EXPECT_TRUE(ArrowChecker::checkEq(expected_batch, actual_batch));
+}
+
+int main(int argc, char** argv) {
+  testing::InitGoogleTest(&argc, argv);
+
+  int err{0};
+  try {
+    err = RUN_ALL_TESTS();
+  } catch (const std::exception& e) {
+  }
+  return err;
+}
