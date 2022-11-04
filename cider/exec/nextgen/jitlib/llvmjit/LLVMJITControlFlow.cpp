d132d6ff1581173d0908ae2d2d800adfb7c071da
jiang1.li <jiang1.li@intel.com>
2022-11-04 10:07:26 +0000

    Fix issue/

--- cider/exec/nextgen/jitlib/llvmjit/LLVMJITControlFlow.cpp
+++ cider/exec/nextgen/jitlib/llvmjit/LLVMJITControlFlow.cpp
@@ -38,72 +38,65 @@ static llvm::Value* castToBool(llvm::IRBuilder<>& builder, llvm::Value* value) {
 }
 
 void LLVMIfBuilder::build() {
-  if (condition_) {
-    auto condition_bb =
-        llvm::BasicBlock::Create(func_.getContext(), ".If_Condition", &func_);
-    builder_.CreateBr(condition_bb);
-    builder_.SetInsertPoint(condition_bb);
+  CHECK(condition_);
+  auto condition_bb =
+      llvm::BasicBlock::Create(func_.getContext(), ".If_Condition", &func_);
+  builder_.CreateBr(condition_bb);
+  builder_.SetInsertPoint(condition_bb);
 
-    JITValuePointer condition_value = condition_();
-    LLVMJITValue& condition_llvm_value = static_cast<LLVMJITValue&>(*condition_value);
+  JITValuePointer condition_value = condition_();
+  LLVMJITValue& condition_llvm_value = static_cast<LLVMJITValue&>(*condition_value);
 
-    auto true_bb = llvm::BasicBlock::Create(func_.getContext(), ".If_True", &func_);
-    auto false_bb = llvm::BasicBlock::Create(func_.getContext(), ".If_False", &func_);
-    auto after_bb = llvm::BasicBlock::Create(func_.getContext(), ".After_If", &func_);
+  auto true_bb = llvm::BasicBlock::Create(func_.getContext(), ".If_True", &func_);
+  auto false_bb = llvm::BasicBlock::Create(func_.getContext(), ".If_False", &func_);
+  auto after_bb = llvm::BasicBlock::Create(func_.getContext(), ".After_If", &func_);
 
-    builder_.CreateCondBr(
-        castToBool(builder_, condition_llvm_value.llvm_value_), true_bb, false_bb);
+  builder_.CreateCondBr(
+      castToBool(builder_, condition_llvm_value.llvm_value_), true_bb, false_bb);
 
-    builder_.SetInsertPoint(true_bb);
-    if (if_true_) {
-      if_true_();
-    }
-    builder_.CreateBr(after_bb);
-
-    builder_.SetInsertPoint(false_bb);
-    if (if_false_) {
-      if_false_();
-    }
-    builder_.CreateBr(after_bb);
-
-    builder_.SetInsertPoint(after_bb);
-  } else {
-    LOG(FATAL) << "Condition builder function is missed in LLVMIfBuilder::build.";
+  builder_.SetInsertPoint(true_bb);
+  if (if_true_) {
+    if_true_();
   }
+  builder_.CreateBr(after_bb);
+
+  builder_.SetInsertPoint(false_bb);
+  if (if_false_) {
+    if_false_();
+  }
+  builder_.CreateBr(after_bb);
+
+  builder_.SetInsertPoint(after_bb);
 }
 
 void LLVMLoopBuilder::build() {
-  if (condition_) {
-    auto condition_bb =
-        llvm::BasicBlock::Create(func_.getContext(), ".For_Condition", &func_);
-    builder_.CreateBr(condition_bb);
-    builder_.SetInsertPoint(condition_bb);
+  CHECK(condition_);
+  auto condition_bb =
+      llvm::BasicBlock::Create(func_.getContext(), ".For_Condition", &func_);
+  builder_.CreateBr(condition_bb);
+  builder_.SetInsertPoint(condition_bb);
 
-    JITValuePointer condition_value = condition_();
-    LLVMJITValue& condition_llvm_value = static_cast<LLVMJITValue&>(*condition_value);
+  JITValuePointer condition_value = condition_();
+  LLVMJITValue& condition_llvm_value = static_cast<LLVMJITValue&>(*condition_value);
 
-    auto for_body = llvm::BasicBlock::Create(func_.getContext(), ".Loop_Body", &func_);
-    auto for_update =
-        llvm::BasicBlock::Create(func_.getContext(), ".Loop_Update", &func_);
-    auto after_for = llvm::BasicBlock::Create(func_.getContext(), ".After_Loop", &func_);
-    builder_.CreateCondBr(
-        castToBool(builder_, condition_llvm_value.llvm_value_), for_body, after_for);
+  auto for_body = llvm::BasicBlock::Create(func_.getContext(), ".Loop_Body", &func_);
+  auto for_update = llvm::BasicBlock::Create(func_.getContext(), ".Loop_Update", &func_);
+  auto after_for = llvm::BasicBlock::Create(func_.getContext(), ".After_Loop", &func_);
+  builder_.CreateCondBr(
+      castToBool(builder_, condition_llvm_value.llvm_value_), for_body, after_for);
 
-    builder_.SetInsertPoint(for_body);
-    if (loop_body_) {
-      loop_body_();
-    }
-    builder_.CreateBr(for_update);
-
-    builder_.SetInsertPoint(for_update);
-    if (update_) {
-      update_();
-    }
-    builder_.CreateBr(condition_bb);
-
-    builder_.SetInsertPoint(after_for);
-  } else {
-    LOG(FATAL) << "Condition builder function is missed in LLVMLoopBuilder::build.";
+  builder_.SetInsertPoint(for_body);
+  if (loop_body_) {
+    loop_body_();
   }
+  builder_.CreateBr(for_update);
+
+  builder_.SetInsertPoint(for_update);
+  if (update_) {
+    update_();
+  }
+  builder_.CreateBr(condition_bb);
+
+  builder_.SetInsertPoint(after_for);
 }
 };  // namespace cider::jitlib
