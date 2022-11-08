9ff2486867621fa560276a3b87fe4195726d2d90
xieqi <qi.xie@intel.com>
2022-11-08 11:29:14 +0800

    [POAE7-2516] Next-gen compiler framework API (#123)
    * [POAE7-2516] Next-gen compiler framework API
    
    * Add lost test file
    
    * Address comment
    
    * add Translator

---
+++ cider/exec/nextgen/OpPipeline.h
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2022 Intel Corporation.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef CIDER_EXEC_NEXTGEN_OP_PIPELINE_H
+#define CIDER_EXEC_NEXTGEN_OP_PIPELINE_H
+
+#include <vector>
+
+#include "exec/nextgen/OpNode.h"
+
+namespace cider::exec::nextgen {
+
+/// \brief An OpPipeline is a linear sequence of relational operators that operate on
+/// tuple data
+class OpPipeline {
+ public:
+  OpPipeline() = default;
+
+  explicit OpPipeline(const OpNodePtrVector& nodes) : nodes_(nodes) {}
+
+  virtual ~OpPipeline() = default;
+
+  /// \brief Register an operator node in this pipeline
+  ///
+  /// \input opNode The operator node to add to the pipeline
+  void appendOpNode(OpNodePtr node) { nodes_.push_back(node); }
+
+  const OpNodePtrVector& getOpNodes() const { return nodes_; }
+
+ private:
+  OpNodePtrVector nodes_;
+};
+
+}  // namespace cider::exec::nextgen
+
+#endif  // CIDER_EXEC_NEXTGEN_OP_PIPELINE_H
---
+++ cider/tests/nextgen/compiler/NextgenCompilerTest.cpp
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2022 Intel Corporation.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <memory>
+
+#include "exec/nextgen/Parser.h"
+#include "exec/nextgen/Transformer.h"
+#include "tests/TestHelpers.h"
+
+TEST(NextgenCompilerTest, FrameworkTest) {
+  using namespace cider::exec::nextgen;
+  RelAlgExecutionUnit eu = {};
+  auto pipeline = Parser::toOpPipeline(eu);
+  auto translator = Transformer::toTranslator(*pipeline);
+}
+
+int main(int argc, char** argv) {
+  TestHelpers::init_logger_stderr_only(argc, argv);
+  testing::InitGoogleTest(&argc, argv);
+  int err = RUN_ALL_TESTS();
+  return err;
+}
