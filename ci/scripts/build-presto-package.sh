c79c0b3595d5a0a313abc8b28b943bba7cb27a97
PenghuiJiao <penghui.jiao@intel.com>
2022-11-15 18:51:26 +0800

    add presto depend (#162)

--- ci/scripts/build-presto-package.sh
+++ ci/scripts/build-presto-package.sh
@@ -17,14 +17,17 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-cd "$(dirname "$0")"
-git clone --recursive https://github.com/Intel-bigdata/presto.git
-pushd presto
-git checkout -b BDTK origin/BDTK
-pushd presto-native-execution
+set -e
+
+PRESTO_CPP_MODE=release
+BDTK_BUILD_MODE=Release
+
+rm -rf presto
+git clone -b BDTK https://github.com/Intel-bigdata/presto.git
+pushd presto/presto-native-execution
 git clone --recursive https://github.com/intel/BDTK.git
-pushd BDTK  
-make release
+pushd BDTK
+make  ${PRESTO_CPP_MODE}
 if [ $? -ne 0 ]; then
     echo "compile BDTK failed"
     exit
@@ -34,27 +37,24 @@ popd
 cp -r ./BDTK/thirdparty/velox .
 mkdir -p ./presto_cpp/main/lib
 
-cp ./BDTK/build-Release/cider-velox/src/libvelox_plugin.a ./presto_cpp/main/lib
-cp ./BDTK/build-Release/cider-velox/src/ciderTransformer/libcider_plan_transformer.a ./presto_cpp/main/lib
-cp ./BDTK/build-Release/cider-velox/src/planTransformer/libvelox_plan_transformer.a ./presto_cpp/main/lib
-cp ./BDTK/build-Release/cider-velox/src/substrait/libvelox_substrait_convertor.a ./presto_cpp/main/lib
-cp -a ./BDTK/build-Release/cider/exec/module/libcider.so* ./presto_cpp/main/lib
-cp ./BDTK/build-Release/thirdparty/velox/velox/substrait/libvelox_substrait_plan_converter.a ./presto_cpp/main/lib
-cp ./BDTK/build-Release/cider/exec/template/libQueryEngine.a ./presto_cpp/main/lib
-cp ./BDTK/build-Release/cider/function/libcider_function.a ./presto_cpp/main/lib
-cp ./BDTK/build-Release/thirdparty/velox/third_party/yaml-cpp/libyaml-cpp.a ./presto_cpp/main/lib
+cp ./BDTK/build-${BDTK_BUILD_MODE}/cider-velox/src/libvelox_plugin.a ./presto_cpp/main/lib
+cp ./BDTK/build-${BDTK_BUILD_MODE}/cider-velox/src/ciderTransformer/libcider_plan_transformer.a ./presto_cpp/main/lib
+cp ./BDTK/build-${BDTK_BUILD_MODE}/cider-velox/src/planTransformer/libvelox_plan_transformer.a ./presto_cpp/main/lib
+cp ./BDTK/build-${BDTK_BUILD_MODE}/cider-velox/src/substrait/libvelox_substrait_convertor.a ./presto_cpp/main/lib
+cp -a ./BDTK/build-${BDTK_BUILD_MODE}/cider/exec/module/libcider.so* ./presto_cpp/main/lib
+cp ./BDTK/build-${BDTK_BUILD_MODE}/thirdparty/velox/velox/substrait/libvelox_substrait_plan_converter.a ./presto_cpp/main/lib
+cp ./BDTK/build-${BDTK_BUILD_MODE}/cider/exec/template/libQueryEngine.a ./presto_cpp/main/lib
+cp ./BDTK/build-${BDTK_BUILD_MODE}/cider/function/libcider_function.a ./presto_cpp/main/lib
 
 sed -i 's/\"planTransformer\/PlanTransformer\.h\"/\"..\/planTransformer\/PlanTransformer\.h\"/' ./BDTK/cider-velox/src/ciderTransformer/CiderPlanTransformerFactory.h
 
-make VELOX_ENABLE_HDFS=ON PRESTO_ENABLE_PARQUET=ON -j ${CPU_COUNT:-`nproc`} release
+make -j ${CPU_COUNT:-`nproc`} PRESTO_ENABLE_PARQUET=ON VELOX_ENABLE_HDFS=ON ${PRESTO_CPP_MODE}
 if [ $? -ne 0 ]; then
     echo "compile presto failed"
     exit
 fi
-mkdir -p ./_build/release/presto_cpp/function
-cp ./BDTK/build-Release/cider/function/RuntimeFunctions.bc ./_build/release/presto_cpp/function/
-
-popd
+mkdir -p ./_build/${PRESTO_CPP_MODE}/presto_cpp/function
+cp ./BDTK/build-${BDTK_BUILD_MODE}/cider/function/RuntimeFunctions.bc ./_build/${PRESTO_CPP_MODE}/presto_cpp/function/
 popd
 
 # build package
@@ -63,50 +63,81 @@ rm -rf ${package_name} ${package_name}.tar.gz
 mkdir -p ${package_name}/lib
 mkdir -p ${package_name}/function
 mkdir -p ${package_name}/bin
-mkdir -p ${package_name}/archive
-cp ./presto/presto-native-execution/_build/release/presto_cpp/function/RuntimeFunctions.bc ./${package_name}/function
+cp -a ./presto/presto-native-execution/_build/${PRESTO_CPP_MODE}/presto_cpp/function/RuntimeFunctions.bc ./${package_name}/function
+cp -a ./presto/presto-native-execution/_build/${PRESTO_CPP_MODE}/presto_cpp/main/presto_server ./${package_name}/bin
 
-DEPS_LIBRARY=(
-    ./presto/presto-native-execution/presto_cpp/main/lib/libcider.so
-    /usr/local/lib/libantlr4-runtime.so
-    /lib/x86_64-linux-gnu/libre2.so
-    /lib/libprotobuf.so
+DEPS_LIBRARY=( 
+    ./presto/presto-native-execution/presto_cpp/main/lib/libcider.so*
+    /usr/local/lib/libantlr4-runtime.so.4.9.3
     /usr/local/lib/libLLVM-9.so
-    /usr/local/lib/libtbb.so
-    /usr/local/lib/libhdfs3.so
-    /usr/lib/x86_64-linux-gnu/libgsasl.so
-    /usr/lib/x86_64-linux-gnu/libntlm.so
-    /lib/x86_64-linux-gnu/libdouble-conversion.so
-    /lib/x86_64-linux-gnu/libglog.so
-    /lib/x86_64-linux-gnu/libgflags.so
-    /lib/x86_64-linux-gnu/libsodium.so
-    /lib/x86_64-linux-gnu/libssl.so
-    /lib/x86_64-linux-gnu/libcrypto.so
-    /lib/x86_64-linux-gnu/libboost_context.so
-    /lib/x86_64-linux-gnu/libevent-2.1.so
-    /lib/x86_64-linux-gnu/libsnappy.so
-    /lib/x86_64-linux-gnu/libboost_regex.so
-    /lib/x86_64-linux-gnu/libboost_locale.so
-    /lib/x86_64-linux-gnu/libboost_log.so
-    /lib/x86_64-linux-gnu/libboost_filesystem.so
-    /lib/x86_64-linux-gnu/libboost_program_options.so
-    /lib/x86_64-linux-gnu/libboost_thread.so
-    /lib/x86_64-linux-gnu/libedit.so
-    /lib/x86_64-linux-gnu/libunwind.so
-    /lib/x86_64-linux-gnu/libicui18n.so
-    /lib/x86_64-linux-gnu/libicuuc.so
-    /lib/x86_64-linux-gnu/libbsd.so
-    /lib/x86_64-linux-gnu/libicudata.so
+    /usr/local/lib/libtbb.so.12
+    /usr/local/lib/libhdfs3.so.1
+    /lib/x86_64-linux-gnu/libglog.so.0
+    /lib/x86_64-linux-gnu/libgflags.so.2.2
+    /lib/x86_64-linux-gnu/libdouble-conversion.so.3
+    /lib/x86_64-linux-gnu/libsodium.so.23
+    /lib/x86_64-linux-gnu/libssl.so.1.1
+    /lib/x86_64-linux-gnu/libcrypto.so.1.1
+    /lib/x86_64-linux-gnu/libboost_context.so.1.71.0
+    /lib/x86_64-linux-gnu/libevent-2.1.so.7
+    /lib/x86_64-linux-gnu/libsnappy.so.1
+    /lib/x86_64-linux-gnu/libboost_regex.so.1.71.0
+    /lib/libprotobuf.so.32
+    /lib/x86_64-linux-gnu/libre2.so.5   
+    /usr/lib/x86_64-linux-gnu/libdouble-conversion.so.3.1
+    /usr/lib/x86_64-linux-gnu/libevent-2.1.so.7.0.0
+    /usr/lib/x86_64-linux-gnu/libgflags.so.2.2.2
+    /usr/lib/x86_64-linux-gnu/libglog.so.0.0.0
+    /usr/local/lib/libhdfs3.so.2.2.31
+    /usr/lib/libprotobuf.so.32.0.4
+    /usr/lib/x86_64-linux-gnu/libre2.so.5.0.0
+    /usr/lib/x86_64-linux-gnu/libsnappy.so.1.1.8
+    /usr/lib/x86_64-linux-gnu/libsodium.so.23.3.0
+    /usr/local/lib/libtbb.so.12.3   
+    /lib/x86_64-linux-gnu/libboost_filesystem.so.1.71.0
+    /lib/x86_64-linux-gnu/libboost_locale.so.1.71.0
+    /lib/x86_64-linux-gnu/libboost_log.so.1.71.0
+    /lib/x86_64-linux-gnu/libboost_program_options.so.1.71.0
+    /lib/x86_64-linux-gnu/libboost_thread.so.1.71.0
+    /lib/x86_64-linux-gnu/libedit.so.2
+    /usr/lib/x86_64-linux-gnu/libedit.so.2.0.63
+    /lib/x86_64-linux-gnu/libgsasl.so.7
+    /usr/lib/x86_64-linux-gnu/libgsasl.so.7.9.7
+    /lib/x86_64-linux-gnu/libunwind.so.8
+    /usr/lib/x86_64-linux-gnu/libunwind.so.8.0.1
+    /lib/x86_64-linux-gnu/libbsd.so.0
+    /usr/lib/x86_64-linux-gnu/libbsd.so.0.10.0
+    /lib/x86_64-linux-gnu/libidn.so.11
+    /usr/lib/x86_64-linux-gnu/libidn.so.11.6.16
+    /lib/x86_64-linux-gnu/libntlm.so.0
+    /usr/lib/x86_64-linux-gnu/libntlm.so.0.0.20
+    /lib/x86_64-linux-gnu/libgssapi_krb5.so.2
+    /lib/x86_64-linux-gnu/libgssapi_krb5.so.2.2
+    /usr/lib/x86_64-linux-gnu/libkrb5.so.3
+    /usr/lib/x86_64-linux-gnu/libkrb5.so.3.3
+    /usr/lib/x86_64-linux-gnu/libk5crypto.so.3
+    /usr/lib/x86_64-linux-gnu/libk5crypto.so.3.1
+    /usr/lib/x86_64-linux-gnu/libkrb5support.so.0
+    /usr/lib/x86_64-linux-gnu/libkrb5support.so.0.1
+    /usr/lib/x86_64-linux-gnu/libkeyutils.so.1
+    /usr/lib/x86_64-linux-gnu/libkeyutils.so.1.8    
+    /usr/lib/x86_64-linux-gnu/libxml2.so.2
+    /usr/lib/x86_64-linux-gnu/libxml2.so.2.9.10
+    /usr/lib/x86_64-linux-gnu/libicui18n.so.66
+    /usr/lib/x86_64-linux-gnu/libicui18n.so.66.1
+    /usr/lib/x86_64-linux-gnu/libicuuc.so.66
+    /usr/lib/x86_64-linux-gnu/libicuuc.so.66.1
+    /usr/lib/x86_64-linux-gnu/libicudata.so.66
+    /usr/lib/x86_64-linux-gnu/libicudata.so.66.1
 )
-cp ./presto/presto-native-execution/_build/release/presto_cpp/main/presto_server ./${package_name}/bin
 
 for lib_prefix in ${DEPS_LIBRARY[@]}
 do
-    cp -a ${lib_prefix}* ./${package_name}/lib
+    cp -a ${lib_prefix} ./${package_name}/lib
 done
 
 tar -czvf ${package_name}.tar.gz ${package_name}
+rm -rf Prestodb
 
 # export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib
 # ./bin/presto_server -etc_dir=./etc
-
