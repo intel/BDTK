0cdf28781db778622e874806cc8f37a45e73e07e
ChaoJun Zhang <chaojun.zhang@intel.com>
2022-11-30 22:05:29 +0800

    [POAE7-2638] Refactor CiderOperator to support new Join Design (#209)
    * [POAE7-2638] Refactor CiderOperator to support new Join Design
    
    * [POAE7-2638] add a new glag to support CiderOperator with new join design
    
    * [POAE7-2638] remove empty line
    
    * [POAE7-2638] fix code style and lint issue
    
    * [POAE7-2638] fix code style and lint issue
    
    * [POAE7-2638] fix license check
    
    * invoke batchProcessor:processNextBatch
    
    * fix compile issue
    
    * fix compile issue
    
    * fix compile issue
    
    * fix compile issue
    
    * fix compile issue
    
    * return blockingReason from batchProcessor::getState
    
    * return blockingReason from batchProcessor::getState
    
    * fix code style

---
+++ cider-velox/src/CiderPipelineOperator.h
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2022 Intel Corporation.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#pragma once
+
+#include "CiderOperator.h"
+#include "CiderPipelineOperator.h"
+#include "cider/processor/BatchProcessor.h"
+
+namespace facebook::velox::plugin {
+
+class CiderPipelineOperator : public CiderOperator {
+ public:
+  CiderPipelineOperator(int32_t operatorId,
+                        exec::DriverCtx* driverCtx,
+                        const std::shared_ptr<const CiderPlanNode>& ciderPlanNode);
+
+  bool needsInput() const override;
+
+  void addInput(RowVectorPtr input) override;
+
+  exec::BlockingReason isBlocked(ContinueFuture* future) override;
+
+  bool isFinished() override;
+
+  RowVectorPtr getOutput() override;
+
+ private:
+  cider::processor::BatchProcessorPtr batchProcessor_;
+};
+}  // namespace facebook::velox::plugin
