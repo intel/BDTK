3b174ea80793015f3babf902032a6f546ef15680
Ma-Jian1 <jian1.ma@intel.com>
2022-12-02 00:38:01 +0000

    remove isNullable

--- cider/exec/nextgen/utils/JITExprValue.h
+++ cider/exec/nextgen/utils/JITExprValue.h
@@ -75,8 +75,6 @@ class JITExprValueAdaptor {
 
   void setNull(jitlib::JITValuePointer& rh) { values_[0].replace(rh); }
 
-  bool isNullable() { return values_[0].get() != nullptr; }
-
  protected:
   JITExprValue& values_;
 };
