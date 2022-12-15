7524e41e9500ef42fa9931656a602ef5500b6d5c
Kunshang Ji <kunshang.ji@intel.com>
2022-12-15 09:09:04 +0800

    add StringHeap and CiderArenaAllocator

--- src/cider/include/cider/CiderAllocator.h
+++ src/cider/include/cider/CiderAllocator.h
@@ -25,6 +25,8 @@
 #include <cstring>
 #include <memory>
 
+#include "cider/CiderException.h"
+
 static constexpr uint16_t kNoAlignment = alignof(max_align_t);
 static constexpr uint16_t kMaxAlignment = 64;
 static constexpr size_t kMaxMemory = std::numeric_limits<size_t>::max();
@@ -111,4 +113,106 @@ class AlignAllocator : public CiderAllocator {
 
   std::shared_ptr<CiderAllocator> parent_;
 };
+
+class AllocatedData {
+ public:
+  AllocatedData(CiderAllocator& allocator, int8_t* pointer, size_t allocated_size)
+      : allocator_(&allocator), pointer_(pointer), allocated_size_(allocated_size) {}
+  ~AllocatedData() { reset(); }
+
+  int8_t* get() { return pointer_; }
+  const int8_t* get() const { return pointer_; }
+  size_t getSize() const { return allocated_size_; }
+  void reset() {
+    if (!pointer_) {
+      return;
+    }
+    allocator_->deallocate(pointer_, allocated_size_);
+    pointer_ = nullptr;
+  }
+
+ private:
+  CiderAllocator* allocator_;
+  int8_t* pointer_;
+  size_t allocated_size_;
+};
+
+struct ArenaChunk {
+  ArenaChunk(CiderAllocator& allocator, size_t size)
+      : current_position(0)
+      , maximum_size(size)
+      , prev(nullptr)
+      , data(allocator, allocator.allocate(size), size){};
+  ~ArenaChunk() {
+    if (next) {
+      auto current_next = std::move(next);
+      while (current_next) {
+        current_next = std::move(current_next->next);
+      }
+    }
+  };
+
+  AllocatedData data;
+  size_t current_position;
+  size_t maximum_size;
+  std::unique_ptr<ArenaChunk> next;
+  ArenaChunk* prev;
+};
+
+class CiderArenaAllocator : public CiderAllocator {
+  static constexpr const size_t ARENA_ALLOCATOR_INITIAL_CAPACITY = 2048;
+
+ public:
+  CiderArenaAllocator(size_t initial_capacity = ARENA_ALLOCATOR_INITIAL_CAPACITY) {
+    head_ = nullptr;
+    tail_ = nullptr;
+    current_arena_capacity_ = initial_capacity;
+  }
+
+  int8_t* allocate(size_t len) final {
+    if (!head_ || head_->current_position + len > head_->maximum_size) {
+      do {
+        current_arena_capacity_ *= 2;
+      } while (current_arena_capacity_ < len);
+      auto new_chunk = std::make_unique<ArenaChunk>(allocator_, current_arena_capacity_);
+      total_capacity_ += current_arena_capacity_;
+      if (head_) {
+        head_->prev = new_chunk.get();
+        new_chunk->next = move(head_);
+      } else {
+        tail_ = new_chunk.get();
+      }
+      head_ = move(new_chunk);
+    }
+    auto result = head_->data.get() + head_->current_position;
+    head_->current_position += len;
+    return result;
+  }
+
+  void deallocate(int8_t* p, size_t size) final {
+    CIDER_THROW(CiderUnsupportedException,
+                "Arena allocator does not support deallocate!");
+  }
+
+  int8_t* reallocate(int8_t* p, size_t size, size_t newSize) final {
+    CIDER_THROW(CiderUnsupportedException,
+                "Arena allocator does not support reallocate!");
+  }
+  size_t getCap() override { return total_capacity_; }
+
+  void destory() {
+    head_ = nullptr;
+    tail_ = nullptr;
+    current_arena_capacity_ = ARENA_ALLOCATOR_INITIAL_CAPACITY;
+    total_capacity_ = 0;
+  }
+
+ private:
+  size_t current_arena_capacity_;
+  size_t total_capacity_ = 0;
+  std::unique_ptr<ArenaChunk> head_;
+  ArenaChunk* tail_;
+  CiderDefaultAllocator allocator_;
+};
+
 #endif
