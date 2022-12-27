be2a7ca9a87981a1b1d97551fbbfc4a7edbe5c8f
Kunshang Ji <kunshang.ji@intel.com>
2022-12-27 15:37:20 +0800

    update ignore order

--- src/cider/tests/nextgen/functional/FilterTest.cpp
+++ src/cider/tests/nextgen/functional/FilterTest.cpp
@@ -154,7 +154,6 @@ TEST_F(CiderFilterRandomTestNG, inTest) {
   assertQuery("SELECT * FROM test WHERE col_2 IS NOT NULL AND col_2 in (24, 25, 26)");
   assertQuery("SELECT * FROM test WHERE col_3 IS NOT NULL AND col_3 in (24, 25, 26)");
   assertQuery("SELECT* FROM test WHERE col_4 IS NOT NULL AND col_4 in (24, 25, 26) ");
-  GTEST_SKIP_("assertQueryIgnoreOrder method not supported yet.");
   assertQueryIgnoreOrder("SELECT * FROM test WHERE col_1 in (24, 25, 26) and col_2 > 20");
   assertQueryIgnoreOrder(
       "SELECT * FROM test WHERE col_1 in (24 * 2 + 2, (25 + 2) * 10, 26)");
@@ -286,7 +285,6 @@ TEST_F(CiderFilterRandomTestNG, multiColRandomTest) {
 }
 
 TEST_F(CiderFilterRandomTestNG, complexFilter) {
-  GTEST_SKIP_("assertQueryIgnoreOrder method not supported yet.");
   assertQueryIgnoreOrder(
       "SELECT * FROM test WHERE (col_1 > 0 AND col_2 < 0) OR (col_1 < 0 AND col_2 > 0)");
 }
