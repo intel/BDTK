0491e42a790e11281511a848ff2d278bb783dea1
PenghuiJiao <penghui.jiao@intel.com>
2022-12-28 15:57:43 +0800

    use velox code

--- ci/scripts/prepare_source_code_cicd.sh
+++ ci/scripts/prepare_source_code_cicd.sh
@@ -18,14 +18,17 @@
 # specific language governing permissions and limitations
 # under the License.
 
+CODE_BASE_PATH=/workspace/code
 PRESTO_LOCAL_PATH=/workspace/github-workspace/presto
 VELOX_COMMIT_ID=`git submodule status -- thirdparty/velox | cut -d' ' -f2`
 echo "velox commit id: ${VELOX_COMMIT_ID}"
 
-pushd thirdparty/velox
+pushd ${CODE_BASE_PATH}/velox
+git pull --rebase
 VELOX_BRANCH_NAME=`git branch -r --contains ${VELOX_COMMIT_ID} | cut -d' ' -f3`
 echo "branch name: ${VELOX_BRANCH_NAME}"
 popd
+
 PRESTO_LOCAL_BRANCH_NAME=`echo ${VELOX_BRANCH_NAME} | cut -d '/' -f2`
 if [ -d $PRESTO_LOCAL_PATH ]; then
     rm -rf $PRESTO_LOCAL_PATH
