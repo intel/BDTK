194d8ac11cc4804b409c1905aad05296bc7bb31a
yma11 <yan.ma@intel.com>
2023-03-03 08:39:30 +0800

    cp substrait related lib

--- ci/scripts/integrate-presto-bdtk.sh
+++ ci/scripts/integrate-presto-bdtk.sh
@@ -59,6 +59,10 @@ cp ${WORKER_DIR}/BDTK/build-${VELOX_PLUGIN_MODE}/cpp/src/cider/exec/nextgen/jitl
 cp ${WORKER_DIR}/BDTK/build-${VELOX_PLUGIN_MODE}/cpp/src/cider/exec/nextgen/libnextgen.a ${WORKER_DIR}/presto_cpp/main/lib
 cp ${WORKER_DIR}/BDTK/build-${VELOX_PLUGIN_MODE}/cpp/src/cider/exec/plan/parser/libcider_plan_parser.a ${WORKER_DIR}/presto_cpp/main/lib
 cp ${WORKER_DIR}/BDTK/build-${VELOX_PLUGIN_MODE}/cpp/src/cider/exec/plan/lookup/libcider_func_lkup.a ${WORKER_DIR}/presto_cpp/main/lib
+cp ./BDTK/build-${BDTK_BUILD_MODE}/cpp/thirdparty/SUBSTRAITCPP/src/SUBSTRAITCPP/substrait/common/libsubstrait_common.a ${WORKER_DIR}/presto_cpp/main/lib
+cp ./BDTK/build-${BDTK_BUILD_MODE}/cpp/thirdparty/SUBSTRAITCPP/src/SUBSTRAITCPP/substrait/function/libsubstrait_function.a ${WORKER_DIR}/presto_cpp/main/lib
+cp ./BDTK/build-${BDTK_BUILD_MODE}/cpp/thirdparty/SUBSTRAITCPP/src/SUBSTRAITCPP/substrait/type/libsubstrait_type.a ${WORKER_DIR}/presto_cpp/main/lib
+cp ./BDTK/build-${BDTK_BUILD_MODE}/cpp/thirdparty/SUBSTRAITCPP/src/SUBSTRAITCPP/third_party/yaml-cpp/libyaml-cpp.a ${WORKER_DIR}/presto_cpp/main/lib
 cp ${WORKER_DIR}/BDTK/build-${VELOX_PLUGIN_MODE}/cpp/src/cider/util/libcider_util.a ${WORKER_DIR}/presto_cpp/main/lib
 cp ${WORKER_DIR}/BDTK/build-${VELOX_PLUGIN_MODE}/cpp/src/cider/type/plan/libcider_type_plan.a ${WORKER_DIR}/presto_cpp/main/lib
 
