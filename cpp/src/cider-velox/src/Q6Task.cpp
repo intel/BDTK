485d07da697301a8d11a5dd686efb086cfffab71
yma11 <yan.ma@intel.com>
2023-03-13 10:33:57 +0800

    POC Q6Task

---
+++ cpp/src/cider-velox/src/Q6Task.cpp
@@ -0,0 +1,161 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "Q6Task.h"
+
+// #include "velox/exec/tests/utils/TpchQueryBuilder.h"
+#include "velox/dwio/common/Options.h"
+#include "velox/dwio/common/ReaderFactory.h"
+#include "velox/tpch/gen/TpchGen.h"
+
+#include "CiderVeloxPluginCtx.h"
+#include "velox/dwio/parquet/RegisterParquetReader.h"
+#include "velox/dwio/parquet/duckdb_reader/ParquetReader.h"
+
+#include <gflags/gflags.h>
+
+using namespace facebook::velox;
+
+using namespace facebook::velox::plugin;
+using facebook::velox::tpch::Table;
+namespace {
+std::string getFormatDateFilter(const std::string& stringDate,
+                                const RowTypePtr& rowType,
+                                const std::string& lowerBound,
+                                const std::string& upperBound) {
+  bool isDwrf = rowType->findChild(stringDate)->isVarchar();
+  auto suffix = isDwrf ? "" : "::DATE";
+
+  if (!lowerBound.empty() && !upperBound.empty()) {
+    return fmt::format(
+        "{} between {}{} and {}{}", stringDate, lowerBound, suffix, upperBound, suffix);
+  } else if (!lowerBound.empty()) {
+    return fmt::format("{} > {}{}", stringDate, lowerBound, suffix);
+  } else if (!upperBound.empty()) {
+    return fmt::format("{} < {}{}", stringDate, upperBound, suffix);
+  }
+
+  VELOX_FAIL("Date range check expression must have either a lower or an upper bound");
+}
+
+void printResult(const RowVectorPtr result) {
+  std::cout << "Results:" << std::endl;
+  bool printType = true;
+  // Print RowType only once.
+  if (printType) {
+    std::cout << result->type()->asRow().toString() << std::endl;
+    printType = false;
+  }
+  for (int32_t i = 0; i < result->size(); ++i) {
+    std::cout << result->toString(i) << std::endl;
+  }
+}
+
+};  // namespace
+
+Q6Task::Q6Task() {
+  CiderVeloxPluginCtx::init();
+  functions::prestosql::registerAllScalarFunctions();
+  parse::registerTypeResolver();
+  filesystems::registerLocalFileSystem();
+  parquet::registerParquetReaderFactory(parquet::ParquetReaderType::NATIVE);
+  const std::string kHiveConnectorId = "test-hive";
+  auto hiveConnector = connector::getConnectorFactory(
+                           connector::hive::HiveConnectorFactory::kHiveConnectorName)
+                           ->newConnector(kHiveConnectorId, nullptr);
+  connector::registerConnector(hiveConnector);
+  std::vector<std::string> selectedColumns = {
+      "l_shipdate", "l_extendedprice", "l_quantity", "l_discount"};
+
+  dwio::common::ReaderOptions readerOptions;
+  readerOptions.setFileFormat(dwio::common::FileFormat::PARQUET);
+  std::unique_ptr<dwio::common::Reader> reader =
+      dwio::common::getReaderFactory(readerOptions.getFileFormat())
+          ->createReader(std::make_unique<dwio::common::FileInputStream>(
+                             "/WorkSpace/nextgen/BDTK/lineitem/lineitem"),
+                         readerOptions);
+  const auto fileType = reader->rowType();
+  const auto fileColumnNames = fileType->names();
+  auto columns = tpch::getTableSchema(tpch::Table::TBL_LINEITEM)->names();
+  // There can be extra columns in the file towards the end.
+  VELOX_CHECK_GE(fileColumnNames.size(), columns.size());
+  std::unordered_map<std::string, std::string> fileColumnNamesMap(columns.size());
+  std::transform(columns.begin(),
+                 columns.end(),
+                 fileColumnNames.begin(),
+                 std::inserter(fileColumnNamesMap, fileColumnNamesMap.begin()),
+                 [](std::string a, std::string b) { return std::make_pair(a, b); });
+  auto columnNames = columns;
+  auto types = fileType->children();
+  types.resize(columnNames.size());
+  auto tableTypes = std::make_shared<RowType>(std::move(columnNames), std::move(types));
+  auto columnSelector =
+      std::make_shared<dwio::common::ColumnSelector>(tableTypes, selectedColumns);
+  auto selectedRowType = columnSelector->buildSelectedReordered();
+  const auto shipDate = "l_shipdate";
+  auto shipDateFilter =
+      getFormatDateFilter(shipDate, selectedRowType, "'1994-01-01'", "'1994-12-31'");
+
+  core::PlanNodeId lineitemPlanNodeId;
+  // auto plan =
+  //     PlanBuilder()
+  //         .tableScan(
+  //             kLineitem,
+  //             selectedRowType,
+  //             fileColumnNames,
+  //             {shipDateFilter, "l_discount between 0.05 and 0.07", "l_quantity
+  //             < 24.0"})
+  //         .capturePlanNodeId(lineitemPlanNodeId)
+  //         .project({"l_extendedprice * l_discount"})
+  //         .partialAggregation({}, {"sum(p0)"})
+  //         .localPartition({})
+  //         .finalAggregation()
+  //         .planNode();
+  auto plan =
+      PlanBuilder()
+          .tableScan("lineitem", selectedRowType, fileColumnNamesMap)
+          .capturePlanNodeId(lineitemPlanNodeId)
+          .filter(shipDateFilter +
+                  " and l_quantity < 24.0 and l_discount >= 0.05 and l_discount <= 0.07")
+          // .filter("l_discount between 0.05 and 0.07")
+          // .filter("l_quantity < 24.0")
+          .project({"l_extendedprice * l_discount"})
+          .partialAggregation({}, {"sum(p0)"})
+          .planFragment();
+  auto rootNode = plan.planNode;
+  plan.planNode = CiderVeloxPluginCtx::transformVeloxPlan(rootNode);
+  task_ = std::make_shared<exec::Task>(
+      "single.execution.task.0", plan, 0, std::make_shared<core::QueryCtx>());
+  auto path = "/WorkSpace/nextgen/BDTK/lineitem/lineitem";
+  // 100 spilts in the file
+  auto const splits = HiveConnectorTestBase::makeHiveConnectorSplits(
+      path, 100, dwio::common::FileFormat::PARQUET);
+  for (const auto& split : splits) {
+    task_->addSplit(lineitemPlanNodeId, exec::Split(split));
+  }
+  task_->noMoreSplits(lineitemPlanNodeId);
+  VELOX_CHECK(task_->supportsSingleThreadedExecution());
+}
+
+RowVectorPtr Q6Task::getOutput() {
+  auto result = task_->next();
+  if (result) {
+    for (auto& child : result->children()) {
+      child->loadedVector();
+    }
+    printResult(result);
+  }
+  return result;
+}
---
+++ cpp/src/cider-velox/src/Q6Task.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "velox/common/base/tests/GTestUtils.h"
+#include "velox/common/file/FileSystems.h"
+#include "velox/common/testutil/TestValue.h"
+#include "velox/connectors/hive/HiveConnector.h"
+#include "velox/connectors/tpch/TpchConnector.h"
+#include "velox/exec/Task.h"
+#include "velox/exec/tests/utils/Cursor.h"
+#include "velox/exec/tests/utils/HiveConnectorTestBase.h"
+#include "velox/exec/tests/utils/PlanBuilder.h"
+#include "velox/functions/prestosql/registration/RegistrationFunctions.h"
+#include "velox/parse/TypeResolver.h"
+
+using namespace facebook::velox;
+using namespace facebook::velox::exec::test;
+
+class Q6Task {
+ public:
+  Q6Task();
+  RowVectorPtr getOutput();
+
+ private:
+  std::shared_ptr<exec::Task> task_;
+};
